4506e8106f8285bb9026f38b3f0393794cec4926
georgemarie <102537518+georgemarie@users.noreply.github.com>
2022-11-15 20:26:31 -0800

    first commit

--- BigRealClass.cpp
+++ BigRealClass.cpp
@@ -1,5 +1,16 @@
 #include "BigRealClass.h"
 
+istream &operator>>(istream &in, BigReal n)
+{
+    in >> n.num;
+    return in;
+}
+
+ostream &operator<<(ostream &out, BigReal n)
+{
+    out << n.get_Number();
+    return out;
+}
 
 BigReal::BigReal(double realNumber = 0.0)
 {
@@ -8,7 +19,15 @@ BigReal::BigReal(double realNumber = 0.0)
 
 BigReal::BigReal(string realNumber)
 {
-    num = realNumber;
+    if (isValid(realNumber))
+    {
+        num = realNumber;
+    }
+    else
+    {
+        cout << "number is invalid" << endl;
+        exit(-1);
+    }
 }
 
 BigReal::BigReal(int realNumber)
@@ -27,125 +46,185 @@ string BigReal::get_Number()
     return num;
 }
 
-// function for + operator
-void makeEqualAtFront(vector<int>& A,
-                      vector<int>& B)
+bool BigReal::isValid(string num)
+{
+    if (num[0] == '-')
+    {
+        sign = 0;
+        num = num.substr(1);
+    }
+    else if (num[0] == '+')
+    {
+        sign = 1;
+        num = num.substr(1);
+    }
+    else
+    {
+        sign = 1;
+    }
+    int count = 0;
+    for (int i = 0; i < num.size(); i++)
+    {
+        if (num[i] == '.')
+        {
+            count += 1;
+        }
+        if (!isdigit(num[i]) && num[i] != '.' && count >= 1)
+            return false;
+    }
+    return true;
+}
+
+void makeEqualAtFront(vector<int> &A, vector<int> &B)
 {
     int n = A.size();
     int m = B.size();
     int diff = abs(n - m);
-   
-    if (n < m) {
-        for (int i = 0; i < diff; i++) {
+
+    if (n < m)
+    {
+        for (int i = 0; i < diff; i++)
+        {
             A.insert(A.begin(), 0);
         }
     }
-    else {
-        for (int i = 0; i < diff; i++) {
+    else
+    {
+        for (int i = 0; i < diff; i++)
+        {
             B.insert(B.begin(), 0);
         }
     }
 }
 
-// function for + operator
-void makeEqualAtback(vector<int>& A,
-                     vector<int>& B)
+void makeEqualAtback(vector<int> &A, vector<int> &B)
 {
     int n = A.size();
     int m = B.size();
     int diff = abs(n - m);
-   
-    if (n < m) {
-        for (int i = 0; i < diff; i++) {
+
+    if (n < m)
+    {
+        for (int i = 0; i < diff; i++)
+        {
             A.push_back(0);
         }
     }
-    else {
-        for (int i = 0; i < diff; i++) {
+    else
+    {
+        for (int i = 0; i < diff; i++)
+        {
             B.push_back(0);
         }
     }
 }
 
-BigReal BigReal::operator+ (BigReal anotherNum)
+BigReal BigReal::operator+(const BigReal &anotherNum)
 {
     int i;
+    string s1 = num;
     string s2 = anotherNum.num;
 
-    vector<int> Intnum, Ints2;
-    vector<int> Fracnum, Fracs2;
-   
-    for (i = num.length() - 1; i > -1; i--) {
-   
-        if (num[i] == '.') {
+    // To store the integer and
+    // fractional part of numbers
+    vector<int> Ints1, Ints2;
+    vector<int> Fracs1, Fracs2;
+
+    // Separating integer and
+    // fractional part of s1
+    for (i = s1.length() - 1; i > -1; i--)
+    {
+
+        // If decimal occurs break
+        if (s1[i] == '.')
+        {
             break;
         }
-        Fracnum.push_back(num[i] - '0');
+        Fracs1.push_back(s1[i] - '0');
     }
-   
+
     i--;
-    for (; i > -1; i--) {
-        Intnum.push_back(num[i] - '0');
+    for (; i > -1; i--)
+    {
+        Ints1.push_back(s1[i] - '0');
     }
-   
-    for (i = s2.length() - 1; i > -1; i--) {
-   
-        if (s2[i] == '.') {
+
+    // Separating integer and
+    // fractional part of s2
+    for (i = s2.length() - 1; i > -1; i--)
+    {
+
+        // If decimal occurs break
+        if (s2[i] == '.')
+        {
             break;
         }
         Fracs2.push_back(s2[i] - '0');
     }
-   
+
     i--;
-    for (; i > -1; i--) {
+    for (; i > -1; i--)
+    {
         Ints2.push_back(s2[i] - '0');
     }
-   
-    makeEqualAtFront(Fracnum, Fracs2);
-    makeEqualAtback(Intnum, Ints2);
 
-    int n = Fracnum.size();
+    // Making number of digits in
+    // fractional and Integer
+    // part equal
+    makeEqualAtFront(Fracs1, Fracs2);
+    makeEqualAtback(Ints1, Ints2);
+
+    // Adding fractional parts of
+    // s1 and s2
+    int n = Fracs1.size();
     int m = Fracs2.size();
     i = 0;
     int carry = 0;
-   
-    while (i < n && i < m) {
-        int sum = Fracnum[i]
-                  + Fracs2[i]
-                  + carry;
-   
-        Fracnum[i] = sum % 10;
+
+    while (i < n && i < m)
+    {
+
+        // Traverse the Fracs1[] and
+        // Fracs2[] and add the digit
+        // and store the carry
+        int sum = Fracs1[i] + Fracs2[i] + carry;
+
+        Fracs1[i] = sum % 10;
         carry = sum / 10;
         i++;
     }
-   
-    int N = Intnum.size();
+
+    int N = Ints1.size();
     int M = Ints2.size();
     i = 0;
-   
-    while (i < N && i < M) {
-        int sum = Intnum[i]
-                  + Ints2[i]
-                  + carry;
-        Intnum[i] = sum % 10;
+
+    // Adding integer part of
+    // s1 and s2
+    while (i < N && i < M)
+    {
+        int sum = Ints1[i] + Ints2[i] + carry;
+        Ints1[i] = sum % 10;
         carry = sum / 10;
         i++;
     }
-
     if (carry != 0)
-        Intnum.push_back(carry);
-  
-    string result = "";
-
-    for (int i = Intnum.size() - 1; i > -1; i--) {
-        result.push_back(Intnum[i]);
+    {
+        Ints1.push_back(carry);
     }
-
-    result.push_back('.');
-
-    for (int i = Fracnum.size() - 1; i > -1; i--) {
-        result.push_back(Fracnum[i]);
+    string r;
+    for (int i = Ints1.size() - 1; i > -1; i--)
+    {
+        string c = "";
+        c += to_string(Ints1[i]);
+        r.push_back(c[0]);
     }
-
+    r.push_back('.');
+    for (int i = Fracs1.size() - 1; i > -1; i--)
+    {
+        string c = "";
+        c += to_string(Fracs1[i]);
+        r.push_back(c[0]);
+    }
+    BigReal result(r);
     return result;
 }
--- BigRealClass.h
+++ BigRealClass.h
@@ -3,22 +3,29 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include "BigDecimalIntClass.h"
 
 using namespace std;
 
 class BigReal
 {
-    private:
-        string num;
-    public:
-        BigReal(double realNumber);
-        BigReal(string realNumber);
-        BigReal(int realNumber);
-        BigReal(BigDecimalInt realNumber);
-        string get_Number();
-        BigReal operator+ (BigReal anothernum);
-};
+private:
+    string num;
+    int sign;
+    int size;
+    bool isValid(string num);
 
+public:
+    BigReal(double realNumber);
+    BigReal(string realNumber);
+    BigReal(int realNumber);
+    BigReal(BigDecimalInt realNumber);
+    string get_Number();
+    BigReal operator+(const BigReal &anothernum);
+    BigReal operator-(const BigReal &anothernum);
+    friend ostream &operator<<(ostream &out, BigReal n);
+    friend istream &operator>>(istream &in, BigReal n);
+};
 
 #endif
